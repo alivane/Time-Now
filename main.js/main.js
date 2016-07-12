692593e668bdaf14b262ed33136b9c5ac6bdfdb0
AliVane <vaneg643@gmail.com>
2016-07-12 12:50:51 -0400

    inicializando en carpetas

---
+++ main.js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function (){
+	var month = ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"];
+	var day = ["Domingo","Lunes","Martes","Miercoles","Jueves","Viernes","Sabado"];
+	var newDate = new Date();
+	newDate.setDate(newDate.getDate()); 
+	
+})
